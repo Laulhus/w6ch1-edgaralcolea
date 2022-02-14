1c254f94c2c65bc025d3276aea153385a92d6822
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-14 21:01:01 +0100

    Added taskReducer and tested loadAction

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -12,4 +12,13 @@ describe("Given an App function", () => {
       expect(heading).toBeInTheDocument();
     });
   });
+
+  describe("When invoked", () => {
+    test("Then it should render a list", () => {
+      render(<App />);
+      const heading = screen.getByRole("list");
+
+      expect(heading).toBeInTheDocument();
+    });
+  });
 });
