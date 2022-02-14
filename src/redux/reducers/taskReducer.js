30633a9bf7e9a2cfc9e3da604a8293bd0353a7a3
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-14 23:43:45 +0100

    Rendered Form component

--- src/redux/reducers/taskReducer.js
+++ src/redux/reducers/taskReducer.js
@@ -2,7 +2,6 @@ import actionTypes from "../actions/actionsTypes";
 
 const taskReducer = (tasks = [], action = {}) => {
   let newTasks;
-  debugger;
   switch (action.type) {
     case actionTypes.loadTasks:
       newTasks = [...action.tasks];
