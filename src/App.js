c218d05905274e35fcbb3997b048920f2c0a8854
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-14 23:36:35 +0100

    Added task rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import Task from "./components/Task/Task";
 import loadThunk from "./redux/thunks/loadThunk";
 
 function App() {
   const dispatch = useDispatch();
+  const tasks = useSelector((state) => state.tasks);
 
   useEffect(() => {
     loadThunk(dispatch);
@@ -13,7 +15,11 @@ function App() {
     <div className="container">
       <h1 className="title">Ed's Redux ToDo</h1>
       <h2 className="list-title">Tasks List:</h2>
-      <ul className="task-list"></ul>
+      <ul className="task-list">
+        {tasks.map((task) => (
+          <Task key={task.id} task={task} />
+        ))}
+      </ul>
     </div>
   );
 }
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,4 +1,4 @@
-const Task = () => {
+const Task = ({ task }) => {
   return <li className="done">Test Task </li>;
 };
 export default Task;
